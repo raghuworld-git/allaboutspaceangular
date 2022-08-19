9b32b605786191c59e5bcba7b33a6b8778f775e0
Raghavendra <72355127+raghuworld-git@users.noreply.github.com>
2022-08-19 17:44:52 +0530

    Launches changes (upcoming and previous )
    Ui changes - Launches changes (upcoming and previous )

--- src/app/features/launches/services/launches.service.ts
+++ src/app/features/launches/services/launches.service.ts
@@ -4,6 +4,7 @@ import { BaseHttpService } from "src/app/core/services/base-http.service";
 import { ILaunchDetail } from "src/app/shared/models/launch/launchDetail.model";
 import { ILaunchStatus } from "src/app/shared/models/launch/launchStatus.model";
 import { ISimpleLaunch } from "src/app/shared/models/launch/SimpleLaunch.model";
+import { IResult } from "src/app/shared/models/result.model";
 import { LaunchUtilService } from "src/app/shared/services/launchUtil.service";
 
 @Injectable()
@@ -21,16 +22,16 @@ export class LaunchesService {
   getLaunchesList(isUpcoming: boolean = true): Observable<ISimpleLaunch[]> {
     if (isUpcoming) {
 
-      return this.baseHttpService.get<ISimpleLaunch[]>(`${this.launchesEndPoint}/GetUpcomingLaunches`).pipe(map((res) => {
-        return res.map((value) => {
+      return this.baseHttpService.get<IResult<ISimpleLaunch>>(`${this.launchesEndPoint}/GetUpcomingLaunches`).pipe(map((res) => {
+        return res.results.map((value) => {
           return { ...value, launchStatusColor: this.launchUtilService.getBadgeColor(value.status?.abbrev) }
         })
       }));
 
     }
     else {
-      return this.baseHttpService.get<ISimpleLaunch[]>(`${this.launchesEndPoint}/GetPreviousLaunches`).pipe(map((res) => {
-        return res.map((value) => {
+      return this.baseHttpService.get<IResult<ISimpleLaunch>>(`${this.launchesEndPoint}/GetPreviousLaunches`).pipe(map((res) => {
+        return res.results.map((value) => {
           return { ...value, launchStatusColor: this.launchUtilService.getBadgeColor(value.status?.abbrev) }
         })
       }));
---
+++ src/app/shared/models/result.model.ts
@@ -0,0 +1,4 @@
+export interface IResult<T> {
+    count: string;
+    results: T[];
+}
